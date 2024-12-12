8548192779755d4df0ae8d770781f09487b52bc7
cbell35 <cara.bell@nisra.gov.uk>
2024-12-12 13:18:46 +0000

    path to data updated to PCOS without year

--- code/path_to_data.R
+++ code/path_to_data.R
@@ -1,4 +1,4 @@
-data_folder <- "B:/SSB/Tech Lab - PCOS 2023/"
+data_folder <- "B:/SSB/Tech Lab - PCOS/"
 #B drive is \\Pr-clus-vfpdfp\DFP_NISRA_Corporate Services\ 
 # (or \\10.27.80.95\DFP_NISRA_Corporate Services\)
 

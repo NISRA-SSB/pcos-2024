805bb6145bb2f926df191b1c2b802f72e59ecc3a
cbell35 <cara.bell@nisra.gov.uk>
2024-12-12 12:51:32 +0000

    file path update

--- code/path_to_data.R
+++ code/path_to_data.R
@@ -1,3 +1,4 @@
-data_folder <- "B:/SSB/Tech Lab - PCOS/"
+data_folder <- "B:/SSB/Tech Lab - PCOS 2023/"
 #B drive is \\Pr-clus-vfpdfp\DFP_NISRA_Corporate Services\ 
 # (or \\10.27.80.95\DFP_NISRA_Corporate Services\)
+

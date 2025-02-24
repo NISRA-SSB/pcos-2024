f81fe5abab1291694a2b3851df185ca44434d3ce
NewUsernamefultonr <ruth.fulton@nisra.gov.uk>
2025-02-24 10:55:54 +0000

    formatting changes to one sheet tables; adding in Note; adding in different table title in text and table for 'Elected bodies' table

--- code/functions/f_embed_ods_timeseries.R
+++ code/functions/f_embed_ods_timeseries.R
@@ -1,8 +1,7 @@
-f_embed_ods_timeseries <- function(df, sheet_title, tab_name, startyear, app_b = FALSE) {
+f_embed_ods_timeseries <- function(df, sheet_title, tab_name, startyear, note = NULL, app_b = FALSE) {
   if (!dir.exists(paste0(here(), "/outputs/table_data"))) {
     dir.create(paste0(here(), "/outputs/table_data"))
   }
-
 
   wb <- createWorkbook(
     creator = "NISRA Statistical Support Branch",
@@ -15,20 +14,36 @@ f_embed_ods_timeseries <- function(df, sheet_title, tab_name, startyear, app_b =
 
   addWorksheet(wb, tab_name)
 
+#note that worksheet contains one table removed, see #'s
+  
   writeData(
     wb, tab_name,
-    c(sheet_title, "This worksheet contains one table.")
+    c(sheet_title)
+  #  c(sheet_title, "This worksheet contains one table.")
   )
 
+  setRowHeights(wb, tab_name, rows = 1, heights = 30)
   addStyle(wb, tab_name,
     style = pt,
     rows = 1,
     cols = 1
   )
 
+  if(!is.null(note)) {
+    writeData(
+#      wb, tab_name, note, startRow =3
+       wb, tab_name, note, startRow =2
+    )
+#   nextRow = 4
+    nextRow = 3
+  } else {
+#   nextRow = 3
+    nextRow = 2
+  }
+  
   writeDataTable(wb, tab_name,
     df,
-    startRow = 3,
+    startRow = nextRow,
     tableStyle = "none",
     withFilter = FALSE,
     tableName = tab_name,
@@ -37,7 +52,7 @@ f_embed_ods_timeseries <- function(df, sheet_title, tab_name, startyear, app_b =
     na.string = "No data"
   )
   
-  r = 3
+  r = nextRow
   
   addStyle(wb, tab_name,
            rows = r,
@@ -100,6 +115,16 @@ f_embed_ods_timeseries <- function(df, sheet_title, tab_name, startyear, app_b =
   )
 
   unlink(xl_name)
+  
+  if (tab_name == "trust_assembly_elected_body") {
+    sheet_title <- paste0("Table 3.3a: Trust in the Northern Ireland Assembly/ Elected Bodies, 2014 to ", current_year)
+  }
+  
+  sheet_title <- if (grepl("\\[Note", sheet_title)) {
+    sub(" \\[.*", "", sheet_title)
+  } else {
+    sheet_title
+  }
 
   if (app_b) {
     paste0(sheet_title, " (", embed_file(ods_name, text = ".ODS format"), "; ", ods_size, ")")

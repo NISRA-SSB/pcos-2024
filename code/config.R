ba987f9a5bb9e62154c12f8b32d64c26cc212789
normabroomfield <norma.broomfield@nisra.gov.uk>
2025-08-01 11:45:35 +0100

    changed publication date

--- code/config.R
+++ code/config.R
@@ -56,7 +56,7 @@ statistic_type <- "os" # options: as  (Accredited Official Statistics),
 
 # Set the report publication data and next publication data if required
 
-pub_date <- "30 June 2025"
+pub_date <- "3 September 2025"
 next_pub_date <- "10 June 2026"
 
 ##### CONTACT DETAILS #####
